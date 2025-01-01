a02180b307b0e399942f172da7bec23732d69a36
valdezalejandra <arvld@icloud.com>
2025-01-01 12:48:48 -0800

    Added Variables

--- macros/variables.sql
+++ macros/variables.sql
@@ -2,4 +2,5 @@
 
     {% set your_name_jinja ="avaldez"%}
     {{log("hello " ~ your_name_jinja, info=true)}}
+    {{log("Hello dbt user " ~ var("user_name","NO USER NAME IS SET") ~ "!", info=true )}}
 {% endmacro %}
